c14c2bdf920b8280f0f3a2db791e0d304009c857
YoichiroKimoto <64562711+YoichiroKimoto@users.noreply.github.com>
2022-01-09 06:20:36 +0900

    Modify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    div.removeChild(completeButton);
+    div.removeChild(deleteButton);
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻す";
+    returnButton.addEventListener("click", () => {
+      div.appendChild(li);
+      div.appendChild(completeButton);
+      div.appendChild(deleteButton);
+      document.getElementById("incomplete-list").appendChild(div);
+      div.removeChild(returnButton);
+    });
+    document.getElementById("complete-list").appendChild(div);
+    div.appendChild(returnButton);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
